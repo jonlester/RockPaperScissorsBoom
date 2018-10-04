d9ce275892887283201ac4fc7ea4b4f06d0b3b1b
Jon Lester <jolest@microsoft.com>
2018-10-04 16:00:53 -0400

    remove authorize, add my bot

--- src/RockPaperScissorsBoom.ExampleBot/Hubs/DecisionHub.cs
+++ src/RockPaperScissorsBoom.ExampleBot/Hubs/DecisionHub.cs
@@ -9,10 +9,28 @@ namespace RockPaperScissorsBoom.ExampleBot.Hubs
 {
     public class DecisionHub : Hub
     {
+        static Guid _matchId = Guid.Empty;
+        static int _round = 0;
         public async Task RequestMove(PreviousDecisionResult previousDecision)
         {
-            var cleverBot = new CleverBot();
-            Decision decision = cleverBot.GetDecision(previousDecision);
+            BaseBot bot;
+            if (previousDecision.MatchId != _matchId)
+            {
+                _matchId = previousDecision.MatchId;
+                _round = 1;
+            }
+
+            if(_round < 100 && previousDecision.OpponentPrevious != Decision.WaterBalloon)
+            {
+                bot = new DynamiteOnlyBot(); 
+            }
+            else
+            {
+                bot = new CleverBot();
+            }
+            _round++;
+
+            Decision decision = bot.GetDecision(previousDecision);
             await Clients.All.SendAsync("MakeDecision", decision);
         }
     }
